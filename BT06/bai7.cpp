17885c53bb76ce17ddce87ded9df2c1fcd35f500
tranthihuyendieu2002 <78572817+tranthihuyendieu2002@users.noreply.github.com>
2021-03-26 00:01:27 +0700

    Add files via upload

---
+++ BT06/bai7.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void xuat(int n, char daymoi[])
+{
+    for (int i = 1; i <= n; i++)
+    {
+        cout << daymoi[i];
+    }
+    cout << endl;
+}
+void thu(int h, int n, int danhdau[], string k, char daymoi[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        if(danhdau[i] == 0)
+        {
+            daymoi[h] = k[i]; danhdau[i] = 1;
+            if (h == n) xuat(n, daymoi);
+            else thu(h + 1, n, danhdau, k, daymoi);
+            danhdau[i] = 0;
+        }
+    }
+}
+int main()
+{
+    int danhdau[1000] = {};
+    char daymoi[1000];
+    string k;
+    cin >> k;
+    int n = k.length();
+    thu(1, n, danhdau, k, daymoi);
+}
